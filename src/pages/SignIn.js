a438d46acbd914da71d2a3ae3dfbf1280a9eeb55
Sathya7032 <116868857+Sathya7032@users.noreply.github.com>
2024-06-18 14:12:55 +0530

    Update SignIn.js

--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -45,7 +45,7 @@ export default function SignIn() {
 
   const handleLoginSuccess = async (response) => {
     try {
-      const res = await axios.post('http://acadamicfolios.pythonanywhere.com/accounts/google/login/token/', {
+      const res = await axios.post('https://acadamicfolios.pythonanywhere.com/accounts/google/login/token/', {
         token: response.credential,  // The token from Google
       });
 
