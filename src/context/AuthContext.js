ce592561fdb44d7e8bc1ec285228d69a02443b18
satyanarayana8179 <acadamicfolio@gmail.com>
2024-06-23 20:35:45 +0530

    newssas

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -27,7 +27,7 @@ export const AuthProvider = ({ children }) => {
     const history = useNavigate();
 
     const loginUser = async (email, password) => {
-        const response = await fetch("https://acadamicfolio.online.com/app/token/", {
+        const response = await fetch("https://acadamicfolio.online/app/token/", {
             method: "POST",
             headers:{
                 "Content-Type":"application/json"
@@ -75,7 +75,7 @@ export const AuthProvider = ({ children }) => {
    
      
     const registerUser = async (email, username, password, password2) => {
-        const response = await fetch("https://acadamicfolio.online.com/app/register/", {
+        const response = await fetch("https://acadamicfolio.online/app/register/", {
             method: "POST",
             headers: {
                 "Content-Type":"application/json"
